20956e2000a3f9dda0728ad525e31ef4fc280b2d
Inbalokesh <inbalokesh.esakkipandi@fwsa.freshworks.com>
2023-01-15 22:32:08 +0530

    Made some pages responsive

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+
+// const name_list = [
+//   {
+//       "name": "Arun",
+//       "image": "https://ui-avatars.com/api/?name=Arun",
+//       "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Est unde quas laborum accusantium molestias velit mollitia alias quia earum repudiandae odio molestiae modi possimus aut, nisi magni quos.Dicta, cupiditate."
+//   },
+//   {
+//       "name": "Ajith",
+//       "image": "https://ui-avatars.com/api/?name=Ajith",
+//       "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Est unde quas laborum accusantium molestias velit mollitia alias quia earum repudiandae odio molestiae modi possimus aut, nisi magni quos.Dicta, cupiditate."
+//   },
+//   {
+//       "name": "Bala",
+//       "image": "https://ui-avatars.com/api/?name=Bala",
+//       "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Est unde quas laborum accusantium molestias velit mollitia alias quia earum repudiandae odio molestiae modi possimus aut, nisi magni quos.Dicta, cupiditate."
+//   },
+//   {
+//       "name": "Deva",
+//       "image": "https://ui-avatars.com/api/?name=Deva",
+//       "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Est unde quas laborum accusantium molestias velit mollitia alias quia earum repudiandae odio molestiae modi possimus aut, nisi magni quos.Dicta, cupiditate."
+//   }
+
+// ]
+
+// name_list[-2] = { "name": "Surya"  };
+
+// name_list[-2]["name"] = "Super star"
+
+// name_list[20] = { "name": "Balu"  };
+
+
+// console.log( name_list ["15"] );
+
+// **********REVERSE TRANIGLE NUMBER**************
+
+
+let n=4;
+
+for (let i = 1; i <= n; i++) {      
+     console.log(" ".repeat(n-i) + "i".repeat(i))
+}    
