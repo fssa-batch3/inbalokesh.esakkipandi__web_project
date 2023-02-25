187e063d138b52c17e5c47e6941d24e33ca53422
Inbalokesh <inbalokesh.esakkipandi@fwsa.freshworks.com>
2023-02-26 00:00:27 +0530

    Completed product Crud

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -6,6 +6,17 @@ function signUp(e) {
         user_address = document.getElementById("user_address").value,
         user_password = document.getElementById("user_password").value,
         confirm_password = document.getElementById("confirm_password").value;
+        
+        function checkPassword(){
+        if (user_password!==confirm_password){
+            alert("Re-enter the Password");
+            confirm_password.value=reset(); 
+        }
+        else{
+            alert("Password Matched");   
+        }
+    }
+    checkPassword();
 
     let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
 
@@ -17,7 +28,7 @@ function signUp(e) {
 
     if(!exist){
         user_list.push({ user_name, user_phonenumber, user_email, user_address, 
-                        user_password, confirm_password });
+                        user_password, confirm_password});
 
         localStorage.setItem('user_list', JSON.stringify(user_list));
 
@@ -27,7 +38,7 @@ function signUp(e) {
         document.getElementById('user_phonenumber').focus();
         document.getElementById('user_email').focus();
         alert('Account created Successfully');
-        location.href = "../../index.html";
+        location.href = "./Login.html";
     }
     
     else{
@@ -37,25 +48,40 @@ function signUp(e) {
     
 }
 
-function signIn(e) {
+function signIn(e){
     e.preventDefault();
     let user_phonenumber = document.getElementById('user_phonenumber').value,
         user_password = document.getElementById('user_password').value;
-    
+
     let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
 
     let exist = user_list.length &&
-    JSON.parse(localStorage.getItem('user_list')).some(data => 
-        data.user_phonenumber.toLowerCase() == user_phonenumber && 
-        data.user_password.toLowerCase() == user_password);
+    JSON.parse(localStorage.getItem('user_list')).some(data =>
+        data.user_phonenumber.toLowerCase() == user_phonenumber &&
+        data.user_password == user_password);
 
     if(!exist){
         alert("Incorrect login credentials");
-        document.querySelector('form').reset();        
+        document.querySelector('form').reset();
     }
     else{
+        localStorage.setItem("userId",JSON.stringify(user_phonenumber));
         alert("Your login in successful");
-        location.href = "/index.html";  
+        location.href = "../../Pages/Login and Order/Profile.html";
     }
 }
+
+// logout toggle function;
+
+// const logIn = document.querySelector(".login");
+// const LogText=document.querySelector("#logOut");
+// LogText.addEventListener("click",logOut)
+
+// function logOut(e){
+// if(logIn.classList.contains(".login")){
+//     logIn.innerHTML="Log Out"
+// }
+// }
+
+
 
