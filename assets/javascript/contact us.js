ee1e27efcb7365674702c2d57888d6b6843c9b9f
Inbalokesh <inbalokesh.esakkipandi@fwsa.freshworks.com>
2023-05-31 18:58:05 +0530

    fix: fixed bugs in contact us page

--- assets/javascript/contact us.js
+++ assets/javascript/contact us.js
@@ -1,6 +1,7 @@
 
 // Contact Us form //
-document.querySelector("#feedback_form").onclick("submit", function mail(e) {
+// console.log(document.querySelector("#feedback_form"));
+document.querySelector("#feedback_form").addEventListener("submit", function mail(e) {
     e.preventDefault();
     let details = {
         name: document.getElementById("name").value,
@@ -8,6 +9,18 @@ document.querySelector("#feedback_form").onclick("submit", function mail(e) {
         email: document.getElementById("email").value,
         message: document.getElementById("message").value
     };
+
+    document.querySelector("div.box").style.display="none";
+    document.querySelector("div.message_gif").style.display="";
+    const gif=`
+        <div class="message_note">
+            <h3>Sending message!</h3>
+        </div>
+        <img src="../../assets/Images/Loading.gif" alt="message gif">
+    `
+
+    document.querySelector(".message_gif").innerHTML=gif;
+
     const serviceId = "service_tgnewnh";
     const templateId = "template_jntl1gg";
 
@@ -15,6 +28,7 @@ document.querySelector("#feedback_form").onclick("submit", function mail(e) {
         .then((res) => {
             document.querySelector("form").reset();
             alert("Your message is sent sucessfully");
+            window.location.reload();
         })
         .catch((err) => console.log(err));
 }
